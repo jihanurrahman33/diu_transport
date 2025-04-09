c6cf3404bcbc11c08b8d81fd294b50dd2cda9be9
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-04-09 23:52:11 +0600

    ui in progress

--- lib/ui/screens/location_screen.dart
+++ lib/ui/screens/location_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-class LocationScreen extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
 
   @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  static const LatLng myLocation = LatLng(23.9004, 90.3272);
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      // body: GoogleMap(
+      //     initialCameraPosition: CameraPosition(target: myLocation, zoom: 15)),
+    );
   }
 }
--- lib/ui/screens/schedule_screen.dart
+++ lib/ui/screens/schedule_screen.dart
@@ -1,9 +1,74 @@
+import 'package:diu_transport/ui/widgets/diu_app_bar.dart';
 import 'package:flutter/material.dart';
+
 class ScheduleScreen extends StatelessWidget {
   const ScheduleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView.builder(
+          itemCount: 4,
+          shrinkWrap: true,
+          primary: false,
+          itemBuilder: (context, index) {
+            return Container(
+                margin: EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: double.maxFinite,
+                decoration: BoxDecoration(color: Color(0xffbccfe0),borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          child: Text('R1'),
+                        ),
+                        Text('Dhanmondi<>DSC(Regular)'),
+                        Icon(Icons.navigation)
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Start time(To DSC)"),
+                    Row(
+                      children: [
+                        Chip(label: Text('07:20 AM')),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Chip(label: Text('07:20 AM')),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Chip(label: Text('07:20 AM')),
+                      ],
+                    ),
+                    Text('Departure Time(From DSC)'),
+                    Row(
+                      children: [
+                        Chip(label: Text('07:20 AM')),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Chip(label: Text('07:20 AM')),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Chip(label: Text('07:20 AM')),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Text(
+                        'Dhanmondi<>Sohanbag<>Shyamoli Square<>Technical Mor<>Majar Road Gabtoli<>Konabari Bus Stop<>Eastern Housing<>Rupnagar<>Birulia Bus Stand<>Daffodil Smart City')
+                  ],
+                ));
+          }),
+    );
   }
 }
